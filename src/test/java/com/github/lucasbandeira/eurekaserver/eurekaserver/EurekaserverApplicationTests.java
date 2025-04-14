e7cc7f01fdcd46cfc4a7da881321f0f73992c031
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-04-14 10:26:59 -0300

    creating the Eureka Server

---
+++ src/test/java/com/github/lucasbandeira/eurekaserver/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasbandeira.eurekaserver.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
